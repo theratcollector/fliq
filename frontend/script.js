0f0be5cee313c86b36627bd2fa36e5cc43729c25
Oskar Tervooren <oskar@Mac-mini-von-Oskar-5.fritz.box>
2025-06-21 19:57:09 +0200

    project struture

---
+++ frontend/script.js
@@ -0,0 +1,5 @@
+const username = "Oskar";
+const friendName = "Daniel";
+
+document.getElementById("greeting").textContent = username;
+document.getElementById("chatUserName").textContent = friendName;
