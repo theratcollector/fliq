dfdd5217eff867c88e91fb2adb705f4e57d4728a
Oskar Tervooren <oskar@MacBook-Air-von-Oskar.fritz.box>
2025-06-29 22:21:49 +0200

    relocated error message in login

--- frontend/login/script.js
+++ frontend/login/script.js
@@ -1,53 +1,53 @@
 function home(){
-        window.location.href="../"
-    }
+    window.location.href="../"
+}
 
-    function signUp(){
-        window.location.href="../register"
-    }
-    
-    const statusMsg = document.getElementById("statusMsg");
+function signUp(){
+    window.location.href="../register"
+}
 
-    const params = new URLSearchParams(window.location.search);
-    const loginStatus = params.get("status");
+const statusMsg = document.getElementById("statusMsg");
 
+const params = new URLSearchParams(window.location.search);
+const loginStatus = params.get("status");
 
-    if(loginStatus === "success"){
-        statusMsg.style.display = "block";
-        statusMsg.innerHTML = "Successfully logged in."
-    }else if(loginStatus === "error"){
-        statusMsg.style.display = "block";
-        statusMsg.innerHTML = "An error occured."
-    }
+if(loginStatus === "success"){
+    statusMsg.style.display = "block";
+    statusMsg.innerHTML = "Successfully logged in."
+    statusMsg.style.color = "#40AA5C"
+}else if(loginStatus === "error"){
+    statusMsg.style.display = "block";
+    statusMsg.innerHTML = "An error occured."
+}
 
-    async function login(){
-        try{
-            const data = {
-                username:document.getElementById("username").value,
-                password:document.getElementById("password").value
-            }
+async function login(){
+    try{
+        const data = {
+            username:document.getElementById("username").value,
+            password:document.getElementById("password").value
+        }
 
-            const res = await fetch("http://localhost:3000/login", {
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json"
-                },
-                body:JSON.stringify(data)
-            });
+        const res = await fetch("http://localhost:3000/login", {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(data)
+        });
 
-            if(!res){
-                console.log("Serverfehler");
-                window.location.href = "?status=error";
-            }
-
-            const result = await res.json();
-
-            if(result.token){
-                localStorage.setItem("token", result.token);
-                window.location.href = "../app";
-            }
-        }catch (err){
-            console.log("Error: "+err.message);
+        if(!res){
+            console.log("Serverfehler");
             window.location.href = "?status=error";
         }
+
+        const result = await res.json();
+
+        if(result.token){
+            localStorage.setItem("token", result.token);
+            window.location.href = "../app";
+        }
+    }catch (err){
+        console.log("Error: "+err.message);
+        window.location.href = "?status=error";
     }
+}
